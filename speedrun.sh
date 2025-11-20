bfa50698f8b1c46ed2f7d167212a47b979013efe
Mateus Costamilan Tomiello <mateus.tomiello@gmail.com>
2025-11-20 19:40:20 -0300

    4 gpu

--- speedrun.sh
+++ speedrun.sh
@@ -13,6 +13,8 @@
 # Default intermediate artifacts directory is in ~/.cache/nanochat
 export OMP_NUM_THREADS=1
 export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat"
+export PATH="$HOME/.local/bin:$PATH"
+
 mkdir -p $NANOCHAT_BASE_DIR
 
 # -----------------------------------------------------------------------------
@@ -83,7 +85,7 @@ echo "Waiting for dataset download to complete..."
 wait $DATASET_DOWNLOAD_PID
 
 # Number of processes/GPUs to use
-NPROC_PER_NODE=8
+NPROC_PER_NODE=4
 
 # pretrain the d20 model
 torchrun --standalone --nproc_per_node=$NPROC_PER_NODE -m scripts.base_train -- --depth=20 --run=$WANDB_RUN
